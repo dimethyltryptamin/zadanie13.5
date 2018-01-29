5f5440324f30b5e5f9cc389d6c270dfa6442b531
dimethyltryptamin <34538318+dimethyltryptamin@users.noreply.github.com>
2018-01-29 19:09:52 +0100

    floor @ sek.

--- modules/time.js
+++ modules/time.js
@@ -2,14 +2,13 @@ var os = require('os');
 
 function liczenie() {
 	var czas = os.uptime();
+	var sekundy = Math.floor(czas % 60);
 	if (czas < 3600) {
 		var minuty = Math.floor(czas / 60);
-		var sekundy = czas % 60;
     return "Czas pracy: " + minuty + " min. " + sekundy + " sek.";
 	} else {
-    var godziny = Math.floor(czas / 3600);
+    	var godziny = Math.floor(czas / 3600);
 		var minuty = Math.floor((czas % 3600) / 60);
-		var sekundy = czas % 60;
     return "Czas pracy: " + godziny + " godz. " + minuty + " min. " + sekundy + " sek.";
 	}
 };
